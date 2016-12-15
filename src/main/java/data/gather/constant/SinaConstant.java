130eb04e50823dce28e41b3a29907644421055fd
unknown <1105406280@qq.com>
2016-12-15 17:53:56 +0800

    抓取 新浪页面

---
+++ src/main/java/data/gather/constant/SinaConstant.java
@@ -0,0 +1,30 @@
+package data.gather.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dell on 2016/12/15.
+ */
+public class SinaConstant {
+
+    public static Map getCookie(){
+        Map<String,String> map = new HashMap();
+        map.put("SINAGLOBAL","7619684792315.096.1479953441245");
+        map.put("SCF","Au8qE7u50RiegYEJXwG-s54WbvilMASeJLGlh3BVrQgoVSHCSQ3wM--KJHQbyOi_eS5UIkWodDXAS5WO5mx6Z4A.");
+        map.put("SUHB","0P1MjoFbmXKXna");
+        map.put("SUB","_2AkMvDGhAdcPhrAZWmvETyWrraoVH-jzEiebBAn7uJhMyAxh87nYgqSVRvq6bhNbLp3bf2E7qkLUNqtWoJg..");
+        map.put("SUBP","0033WrSXqPxfM72wWs9jqgMF55529P9D9WW_uLolSRXsZLFBMink1jFN5JpVF02f1h5peK5E1K.f");
+        map.put("login_sid_t","6709f8d319a801802466c73e155fdc1c");
+        map.put("YF-Ugrow-G0","8751d9166f7676afdce9885c6d31cd61");
+        map.put("YF-V5-G0","5f9bd778c31f9e6f413e97a1d464047a");
+        map.put("_s_tentry","-");
+        map.put("Apache","2524851675696.114.1481765998783");
+        map.put("ULV","1481765998787:1:1:1:2524851675696.114.1481765998783:");
+        map.put("YF-Page-G0","734c07cbfd1a4edf254d8b9173a162eb");
+        map.put("WBStorage","2c466cc84b6dda21|undefined");
+        return map;
+    }
+
+
+}
--- src/main/java/data/gather/server/SinaService.java
+++ src/main/java/data/gather/server/SinaService.java
@@ -1,20 +1,27 @@
 package data.gather.server;
 
-import java.util.Set;
-import java.util.regex.Pattern;
-
-//import com.yoyo.spider.utils.HttpTools;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.google.common.collect.Sets;
+import data.gather.constant.SinaConstant;
+import data.gather.util.AbstractGather;
+import data.gather.util.JsonObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import com.google.common.collect.Sets;
+
+import java.util.Map;
+import java.util.Set;
+
+//import com.yoyo.spider.utils.HttpTools;
 
 /**
  * Created by dell on 2016/12/15.
  */
 public class SinaService {
 
+    private static JsonObjectMapper jsonObjectMapper = new JsonObjectMapper();
     private static final String PREPAGE = "http://weibo.com/?category=7";//按字母查找品牌 车系 数据时 拼接品牌链接前缀
 //    private static final String TAILHTML = ".html";//按字母查找品牌 车系 数据时 拼接品牌链接后缀
 //    private static final String TEXTFLAG = "暂无";//没有据字样
@@ -24,19 +31,49 @@ public class SinaService {
     public static Set<String> getBrandPages(){
         Set<String> set = Sets.newHashSet();
         try {
-            Document doc = Jsoup.connect(PREPAGE).get();
-            System.out.println(doc.text());
-//            Elements eles = doc.select("div.pic_txt clearfix");
+            Map<String, String> cookies = SinaConstant.getCookie();
+            Document doc = Jsoup.connect(PREPAGE).cookies(cookies).get();
+//            System.out.println(doc);
+            Elements eles = doc.getElementsByTag("script");
 //            System.out.println("--"+eles.text());
-//            for (Element ele : eles) {
-//                set.add(ele.text());
-//            }
+            for (Element ele : eles) {
+                if(ele.toString().contains("\"ns\":\"pl_unlogin_home_feed\"")){
+                    String html = ele.toString() .replaceAll("<script charset=\"utf-8\">", "").replaceAll("<script>","");
+                    html = AbstractGather.getMessage(html);
+                    Map map = jsonObjectMapper.readValue(html, Map.class);
+                    Object htmls =  map.get("html");
+                    System.out.println(htmls.toString());
+                    Document docChild = Jsoup.parse(html);
+                    Elements elesChild = docChild.getElementsByClass("text text_cut2 W_f14");
+                    for (Element eleChild : elesChild){
+                        System.out.println("-----------------------------------");
+                        System.out.println(eleChild.text());
+                    }
+//                    AbstractGather.getHtml(html);
+                }
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return set;
     }
 
+    public static void test(){
+        WebClient webClient = new WebClient(BrowserVersion.getDefault());
+
+    }
+
+//    public void getHtml(){
+//        /** HtmlUnit请求web页面 */
+//        WebClient wc = new WebClient(BrowserVersion.CHROME);
+//        wc.getOptions().setUseInsecureSSL(true);
+//        wc.getOptions().setJavaScriptEnabled(true); // 启用JS解释器，默认为true
+//        wc.getOptions().setCssEnabled(false); // 禁用css支持
+//        wc.getOptions().setThrowExceptionOnScriptError(false); // js运行错误时，是否抛出异常
+//        wc.getOptions().setTimeout(100000); // 设置连接超时时间 ，这里是10S。如果为0，则无限期等待
+//        wc.getOptions().setDoNotTrackEnabled(false);
+//    }
 
 //    // 获取某个字母下的品牌车 下的 所有车系id
 //    private Set<String> gatherCarsIds(String url) {
