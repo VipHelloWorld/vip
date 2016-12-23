97c1b3fdce7373daee0c89461e94cf194b949354
unknown <1105406280@qq.com>
2016-12-23 14:11:31 +0800

    change theam

---
+++ src/main/java/com/onwer/controller/InfoController.java
@@ -0,0 +1,12 @@
+package com.onwer.controller;
+
+/**
+ * Created by dell on 2016/12/23.
+ */
+public class InfoController {
+
+    public void queryCount(){
+//        String json = new
+
+    }
+}
---
+++ src/main/java/com/onwer/dao/InfoDao.java
@@ -0,0 +1,11 @@
+package com.onwer.dao;
+
+import java.util.List;
+
+/**
+ * Created by dell on 2016/12/23.
+ */
+public interface InfoDao {
+//    List list count = new List
+}
+
